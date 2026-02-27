45539aaba0230488c88f454c369f8c00ff48ae50
developeryaseen57 <developeryaseen57@gmail.com>
2026-02-27 12:22:35 +0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("yaseen is working on branch devnode");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("application", "root", "786Yaseen.", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+const People = sequelize.define("People", {
+  name: DataTypes.STRING,
+  email: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  gender: DataTypes.ENUM("male", "female", "other")
+});
+
+async function run() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected successfully");
+
+    await sequelize.sync();
+    console.log("Tables synced");
+
+    await People.create({
+      name: "Yaseen",
+      email: "yaseen@gmail.com",
+      gender: "male"
+    });
+
+    await People.create({
+      name: "Younas",
+      email: "younas@gmail.com",
+      gender: "male"
+    });
+
+    await People.create({
+      name: "Yousaf",
+      email: "yousaf@gmail.com",
+      gender: "male"
+    });
+
+    console.log("Users inserted");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+run();
+
+
+console.log("yaseen is working");
